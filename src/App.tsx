23a2fd4f1b06da0f6a1f5ac1e70ee3da83a59207
Tony Yin <tonyyin@udel.edu>
2026-02-04 14:37:21 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                Edit <code>Tony Yin dlajfs;dlfjas;lkdf</code> and save. This page will
                 automatically reload.
             </p>
         </div>
